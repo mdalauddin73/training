99770280208f6bdc608a2ca34754fb8d28162acd
Mohammad Alauddin <alaumoon@gmail.com>
2022-08-31 13:13:51 +0600

    deleted some

--- ethernet-cabling.php
+++
@@ -1,8 +0,0 @@
-
-
-
-DO														Don't
-Use differnet color schemes								Run cable in unsafe locations
-Label the cable											Over-Cable your racks
-Use an effective wire management system					Leave wire esposed by terminations
-Purchase connectorized cable (Patch Cable)
